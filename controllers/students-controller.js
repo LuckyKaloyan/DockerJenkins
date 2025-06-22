cda2d163c7a058737800e34abe3bf2b04dfe44fc
Калоян Димитров <luckykaloyan@gmail.com>
2025-06-22 22:47:04 +0300

    2nd try

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -1,7 +1,7 @@
 function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
-      title: "MVC Examples",
+      title: "MVC Exampless",
       msg: "Students Registry",
       students: students
     };
